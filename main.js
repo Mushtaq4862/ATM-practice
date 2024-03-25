9d29066a8c920c01e484918b0eba503374c3b99d
Mushtaq4862 <160068543+Mushtaq4862@users.noreply.github.com>
2024-03-26 00:12:43 +0500

    my second assignment
    my second assignment for practice and leaning your self, if any suggestion or advice please tell me. thank you

---
+++ main.js
@@ -0,0 +1,43 @@
+// let a: number = 5
+// let b: number = 2
+// let c: number;
+// c = ++a + a++ + --b + b-- + a + b++ + b
+// console.log("c:", c);
+import inquirer from "inquirer";
+const answers = await inquirer.prompt([
+    {
+        type: "number",
+        name: "numberOne",
+        message: "kindly enter your first no:"
+    },
+    {
+        type: "number",
+        name: "numberTwo",
+        message: "kindly enter your second no:"
+    },
+    {
+        type: "list",
+        name: "operator",
+        choices: ["*", "+", "-", "/"],
+        message: "select operator:"
+    }
+]);
+const { numberOne, numberTwo, operator } = answers;
+if (numberOne && numberTwo && operator) {
+    let result = 0;
+    if (operator === "+") {
+        result = numberOne + numberTwo;
+    }
+    else if (operator === "-") {
+        result = numberOne - numberTwo;
+    }
+    if (operator === "/") {
+        result = numberOne / numberTwo;
+    }
+    if (operator === "*") {
+        result = numberOne * numberTwo;
+    }
+    console.log("your result is :", result);
+}
+else {
+}
